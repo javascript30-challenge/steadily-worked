7ca7f2b077da0e7c3dfbf1e44c0c53b113b6f027
steadily-worked <iamsangminpark@gmail.com>
2022-04-07 21:53:34 +0900

    feat: clock

---
+++ 02-clock/index.ts
@@ -0,0 +1,22 @@
+const hourHand: HTMLDivElement = document.querySelector(".hour-hand");
+const minHand: HTMLDivElement = document.querySelector(".min-hand");
+const secondHand: HTMLDivElement = document.querySelector(".second-hand");
+
+function setTime() {
+  const clock = new Date();
+  const hours = clock.getHours();
+  const minutes = clock.getMinutes();
+  const seconds = clock.getSeconds();
+
+  const degreeOfSeconds = (seconds / 60) * 360 + 90;
+  const degreeOfMinutes = (minutes / 60) * 360 + 90;
+  const degreeOfHours = (hours / 12) * 360 + 90;
+
+  hourHand.style.transform = `rotate(${degreeOfHours}deg)`;
+  minHand.style.transform = `rotate(${degreeOfMinutes}deg)`;
+  secondHand.style.transform = `rotate(${degreeOfSeconds}deg)`;
+}
+
+setInterval(setTime, 1000);
+
+setTime();
